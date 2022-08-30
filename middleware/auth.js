b40d0a264e4cecd47521183eb4616f37314d5991
Daniel Ilies <iliesh.daniel@gmail.com>
2022-08-30 15:28:23 +0300

    update split

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,11 @@
 import jwt, { decode } from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
-  const token = req.headers.authorization.split('Bearer ')[1];
+  const token = req.headers.authorization?.split("Bearer ")[1];
   const isCustomAuth = token.length < 500;
   try {
-
     let decodedData;
-    
+
     if (token && isCustomAuth) {
       decodedData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
@@ -18,23 +17,24 @@ const auth = async (req, res, next) => {
     }
 
     next();
-
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export const signRefreshToken = (req) => {
   try {
     const getToken = req.cookies.refreshToken;
     if (getToken) {
       const { id } = jwt.verify(getToken, process.env.REFRESH_TOKEN_SECRET);
-      const accesssToken = jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '90d' });
-      return { accesssToken }
+      const accesssToken = jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "90d",
+      });
+      return { accesssToken };
     }
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export default auth;
