a1a468b9e4da22619d0b6335f2e5e4e7e1643afc
Daniel Ilies <daniel.ilies@icloud.com>
2023-01-20 02:20:41 +0200

    configured cors

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ithub-mocha.vercel.app"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://ithub-mocha.vercel.app"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Routes
 app.use("/post", postRoutes);
